4fd6d48627438a0e94c512881d0bcd06dd032a17
Mustafa Mohamed <Mustafa>
2021-05-02 16:23:32 +0100

    updated

--- src/api/services/products/products.js
+++ src/api/services/products/products.js
@@ -87,6 +87,7 @@ exports.createProduct = async (jwtClaim, payload, image) => {
     )
   }
   const { destination, mimetype, path, size, filename } = image;
+  console.log(image)
   const pathToFile = `${destination}/${filename}`;
   const formData = new FormData();
   formData.append("image", fs.createReadStream(pathToFile), filename);
